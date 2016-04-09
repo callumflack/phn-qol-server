6438b2c1788522f130f7aedc15603929f71dbe40
Kashi Samaraweera <kashi@kashmaniac.com.au>
2016-04-09 13:39:12 +1000

    Implemented mocha testing with basic Express route test.

---
+++ test/routes/home.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var request = require('supertest');
+var projectPath = path.join(__dirname, '../../');
+var app = require(path.join(projectPath, './app.js'));
+
+describe('Running Express', function () {
+    it('responds to base URL \'/\'', function(done) {
+        request(app)
+            .get('/')
+            .expect(200)
+            .end(function(err, res) {
+                done();
+            });
+    });
+});
