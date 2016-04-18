bc4ef121679263b1015eda485f8e172d6ed1c6c5
Kashi Samaraweera <kashi@kashmaniac.com.au>
2016-04-19 01:27:08 +1000

    Added basic device registration route.

---
+++ routes/device.js
@@ -0,0 +1,38 @@
+/**
+ * Device route
+ * 
+ * This Express.js router manages device registration for the PHN QoL Survey.
+ * Methods GET, POST and DELETE are used to manage devices, identified and
+ * authenticated using JWT tokens.
+ * 
+ * @author Kashi Samaraweera <kashi@kashis.com.au>
+ * @version 0.1.0
+ */
+
+var express = require('express');
+var router = express.Router();
+
+router
+    .get(
+        '/',
+        /**
+         * Returns the data stored in the JWT for the requestion device.
+         * @unimplmented
+         */
+        (req, res, next) => {
+            res.json({ a: "Please authenticate." });
+        }
+    ).post(
+        '/',
+        /**
+         * Device registration.
+         * 
+         * Accepts device information in order to register it in the database.
+         */
+        (req, res, next) => {
+            res.json({ a: "Thank you for your submission." });
+        }
+    );
+
+
+module.exports = router;
---
+++ test/integration/routes/device.js
@@ -0,0 +1,49 @@
+/**
+ * Device reistration REST endpoint tests
+ * 
+ * Examines the `/survey` REST endpoint to confirm that it functions correctly.
+ * 
+ * @author Kashi Samaraweera <kashi@kashis.com.au>
+ * @version 0.1.0
+ */
+// import path from 'path';
+// import request from 'supertest';
+// import chai from 'chai';
+
+// const assert = chai.assert;
+// const projectPath = path.join(__dirname, '../../../');
+// const app = require(path.join(projectPath, './app.js'));
+
+// var responseBody;
+
+// module.exports = function() {
+//     describe('Survey endpoint', function() {
+//         it('HTTP1.1/GET `/surey`', (done) => {
+//             this.timeout(5000);
+//             request(app)
+//                 .get('/survey')
+//                 .expect(200)
+//                 .expect('Content-Type', /json/)
+//                 .end(function(err, res) {
+//                     responseBody = res.body;
+//                     assert.isNull(err);
+//                     assert.isObject(responseBody);
+//                     done();
+//                 });
+//         });
+        
+//         it('HTTP1.1/POST `/surey`', (done) => {
+//             this.timeout(5000);
+//             request(app)
+//                 .post('/survey')
+//                 .expect(200)
+//                 .expect('Content-Type', /json/)
+//                 .end(function(err, res) {
+//                     responseBody = res.body;
+//                     assert.isNull(err);
+//                     assert.isObject(responseBody);
+//                     done();
+//                 });
+//         });        
+//     });
+// }
