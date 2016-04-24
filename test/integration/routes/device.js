742aa0efad2ecc785b912a8a2d368534bd3cbd6d
Kashi Samaraweera <kashi@kashmaniac.com.au>
2016-04-24 12:09:41 +1000

    Device REST endpoint POST testing implemented.

--- test/integration/routes/device.js
+++ test/integration/routes/device.js
@@ -15,6 +15,8 @@ const assert = chai.assert;
 const projectPath = path.join(__dirname, '../../../');
 const app = require(path.join(projectPath, './app.js'));
 
+const TEST_PROVIDER_CODE = "ENT101";
+
 var responseBody;
 
 module.exports = function() {
@@ -33,16 +35,15 @@ module.exports = function() {
                 });
         });
         
-        it('HTTP1.1/POST `/device`', (done) => {
+        it('HTTP1.1/POST empty `/device` returns 400', (done) => {
             this.timeout(5000);
             request(app)
                 .post('/device')
-                .expect(200)
+                .expect(400)
                 .expect('Content-Type', /json/)
                 .end(function(err, res) {
                     responseBody = res.body;
                     assert.isNull(err);
-                    assert.isObject(responseBody);
                     done();
                 });
         });
@@ -60,5 +61,44 @@ module.exports = function() {
                     done();
                 });
         });        
+        
+        it('Validation: valid providerCode', (done) => {
+            this.timeout(5000);
+            request(app)
+                .post('/device')
+                .send({
+                    providerCode: TEST_PROVIDER_CODE,
+                    ipAddress: '192.168.0.1',
+                    userAgent: 'Mocha test framework.'
+                })
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end(function(err, res) {
+                    responseBody = res.body;
+                    assert.isObject(responseBody);
+                    done();
+                });
+        });
+        
+        it('Validation: invalid providerCode raises not_found', (done) => {
+            this.timeout(5000);
+            request(app)
+                .post('/device')
+                .send({
+                    providerCode: TEST_PROVIDER_CODE + 'laslda',
+                    ipAddress: '192.168.0.1',
+                    userAgent: 'Mocha test framework.'
+                })
+                .expect(400)
+                .expect('Content-Type', /json/)
+                .end(function(err, res) {
+                    responseBody = res.body;
+                    assert.isObject(responseBody);
+                    assert.isArray(responseBody.errors);
+                    assert.equal("providerCode", responseBody.errors[0].input);
+                    assert.equal("not_found", responseBody.errors[0].code);
+                    done();
+                });
+        });
     });
 }
